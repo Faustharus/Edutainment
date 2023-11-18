009661123c90747424ded00e1193e27eae5b1d3a
Faustharus <chailloleau-damien@outlook.fr>
2023-11-18 16:55:07 +0100

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Damien Chailloleau on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
